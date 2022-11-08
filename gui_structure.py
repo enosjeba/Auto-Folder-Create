bd785f03fcacebabe67f81816af21d63ae66d456
Enosjeba <enosjeba@gmail.com>
2022-11-08 22:17:14 +0530

    Update gui_structure.py

--- gui_structure.py
+++ gui_structure.py
@@ -5,31 +5,11 @@ root = Tk()
 # fg="color name"
 # bg="color name"
 
-title = Label(root, text="Folder Bag")
-title.pack()
-
-sub_title = Label(root, text="Start Your project")
-sub_title.pack()
-
-#Entry
-project_name_inp = Entry(root)
-project_name_inp.insert(0,"Enter Project Name")
-project_name_inp.pack()
-
-
-#Input Data
-def inputs():
-    project_name = project_name_inp.get()
-    project_name_prnt = Label(root, text=project_name+ " created")
-    project_name_prnt.pack()
-
 #Button
 # Additional Options
 # command=function
 btn_create = Button(root, text="Create Project", command=inputs)
 btn_create.pack()
-
-
 
 #Button
 # Additional Options
