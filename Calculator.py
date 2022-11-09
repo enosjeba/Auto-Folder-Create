b2b8a1a4921add7ff01ed1b8f79c51bb1b139406
Enosjeba <enosjeba@gmail.com>
2022-11-09 22:12:28 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -84,6 +84,11 @@ button_equal = Button(root, text="=", padx = 35, pady=35, command=button_equal)
 
 #Add to screen
 button_1.grid(row = 1, column = 0)
+button_2.grid(row = 1, column = 1)
+button_3.grid(row = 1, column = 2)
+button_4.grid(row = 2, column = 0)
+button_5.grid(row = 2, column = 1)
+button_6.grid(row = 2, column = 2)
 button_7.grid(row = 3, column = 0)
 button_8.grid(row = 3, column = 1)
 button_9.grid(row = 3, column = 2)
