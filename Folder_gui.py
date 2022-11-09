9f57588594aad3fe9ccdbf645c1142b996c512bc
Enosjeba <enosjeba@gmail.com>
2022-11-09 23:53:01 +0530

    Update Folder_gui.py

--- Folder_gui.py
+++ Folder_gui.py
@@ -9,42 +9,50 @@ folder_num = Toplevel()
 title = Label(folder_num, text="Folder Names")
 title.pack()
 
-#Project Name
+# Project Name
 project_name = Entry(root)
 project_name.insert(0,"Enter Project Name")
 project_name.pack()
 
-#Number of Folders
-number_of_folders = Entry(folder_num)
-number_of_folders.insert(0,"Enter Number of Folders")
+# Number of Folders
+number_of_folders = Entry(folder_num, width=35)
 number_of_folders.pack()
 
 folder_names = []
+
+def button_click():
+    number_of_folders.get()
+    print(type(number_of_folders))
+
+button_1 = Button(folder_num, text="Submit", width=35 , command=button_click).pack()
 
 # def project_folder(project_name):
 #     os.mkdir(f'./{project_name}/')
 #     os.chdir(f'./{project_name}/')
 
-#change working directory to project folder
+# change working directory to project folder
 
 # def create_folder(folder_name, number):
 #     os.makedirs(f'./{folder_name}/')
 
-if number_of_folders > 0:
-    os.mkdir(f'./{project_name}/')
-    os.chdir(f'./{project_name}')
-    print(f'You are in {project_name} folder')
-    while True:  
-        a = len(folder_names)+1
-        data = input(f'Enter Sub-Folder {a} Name ')
-        folder_names.append(data)
-        if len(folder_names) == number_of_folders:
-            print("Number of Folders : ", len(folder_names))
-            print(folder_names)
-            break
-else:
-    print("Check inputs")
 
-for i in folder_names:
-    os.mkdir(f'./{i}/')
-    print(f'created folder {i}')
+# if number_of_folders > 0:
+#     os.mkdir(f'./{project_name}/')
+#     os.chdir(f'./{project_name}')
+#     print(f'You are in {project_name} folder')
+#     while True:  
+#         a = len(folder_names)+1
+#         data = input(f'Enter Sub-Folder {a} Name ')
+#         folder_names.append(data)
+#         if len(folder_names) == number_of_folders:
+#             print("Number of Folders : ", len(folder_names))
+#             print(folder_names)
+#             break
+# else:
+#     print("Check inputs")
+
+# for i in folder_names:
+#     os.mkdir(f'./{i}/')
+#     print(f'created folder {i}')
+
+root.mainloop()
